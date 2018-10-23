fbfd2bec16bd2f2ef812a66432dd4f68b879810f
Travis Nguyen <travisn714@gmail.com>
2018-10-23 10:57:37 -0700

    Add a plus icon to each meal time. #3

--- src/home.js
+++ src/home.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
-import { Card, CardTitle } from 'reactstrap'
+import { Card, CardTitle, Button } from 'reactstrap'
+import Icon from '@material-ui/core/Icon'
+
+const styles = {
+  icon: {
+    color: '#78C1AD'
+  }
+}
 
 export default class Home extends React.Component {
   render() {
@@ -12,16 +19,24 @@ export default class Home extends React.Component {
         alignItems="center">
         <div className="mt-5">
           <Card body className="shadow sm text-center m-4">
-            <CardTitle className="h6">Add Breakfast</CardTitle>
+            <CardTitle className="h6">Add Breakfast
+              <i className="fa fa-plus-circle ml-2" style={styles.icon}></i>
+            </CardTitle>
           </Card>
           <Card body className="shadow sm text-center m-4">
-            <CardTitle className="h6">Add Lunch</CardTitle>
+            <CardTitle className="h6">Add Lunch
+              <i className="fa fa-plus-circle ml-2" style={styles.icon}></i>
+            </CardTitle>
           </Card>
           <Card body className="shadow sm text-center m-4">
-            <CardTitle className="h6">Add Dinner</CardTitle>
+            <CardTitle className="h6">Add Dinner
+              <i className="fa fa-plus-circle ml-2" style={styles.icon}></i>
+            </CardTitle>
           </Card>
           <Card body className="shadow sm text-center m-4">
-            <CardTitle className="h6">Add Snacks</CardTitle>
+            <CardTitle className="h6">Add Snacks
+              <i className="fa fa-plus-circle ml-2" style={styles.icon}></i>
+            </CardTitle>
           </Card>
         </div>
       </Grid>
