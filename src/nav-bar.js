8902e64d33759eb799b7e17d20a58a4d4da59ded
Travis Nguyen <travisn714@gmail.com>
2018-10-30 16:28:57 -0700

    Add a History tab that takes user to history view when clicked. #7

--- src/nav-bar.js
+++ src/nav-bar.js
@@ -14,9 +14,12 @@ export default function NavBar() {
         <NavbarBrand href="#home" style={styles.navBar}>
           <i className="fas fa-cookie-bite mr-2"></i>
         CalPal</NavbarBrand>
-        <Nav className="ml-auto" navbar>
+        <Nav className="ml-2" navbar>
           <NavItem>
             <NavLink href="#home" style={styles.navBar}>Home</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#history" style={styles.navBar}>History</NavLink>
           </NavItem>
         </Nav>
       </Navbar>
