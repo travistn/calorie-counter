ee8a67386e87d0b009b78ef600399e2f9c5f81cf
Travis Nguyen <travisn714@gmail.com>
2018-11-01 13:14:01 -0700

    Define a Progress component that displays a graph. #8

---
+++ src/progress.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Line } from 'react-chartjs-2'
+
+const data = {
+  labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  datasets: [
+    {
+      label: 'Calories Consumed',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [65, 59, 80, 81, 56, 55, 40]
+    }
+  ]
+}
+
+export default class Chart extends React.Component {
+  render() {
+    return (
+      <div>
+        <Line data={data}/>
+      </div>
+    )
+  }
+}
