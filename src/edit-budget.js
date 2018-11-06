91acadf7304ec6a6630def36f23b9bff12604066
Travis Nguyen <travisn714@gmail.com>
2018-11-06 15:27:47 -0800

    Create an EditBudget component that takes a form. #22

---
+++ src/edit-budget.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import FormHelperText from '@material-ui/core/FormHelperText'
+import TextField from '@material-ui/core/TextField'
+import { Form, FormGroup, Button } from 'reactstrap'
+
+export default class EditBudget extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      weight: '',
+      calorieGoal: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleChange(event) {
+    const form = event.target
+    this.setState({
+      [form.name]: parseInt(form.value, 10)
+    })
+  }
+  handleSubmit(event) {
+    event.preventDefault()
+    const user = Object.assign({}, this.state)
+    this.props.onSubmit(user)
+  }
+  render() {
+    const { value } = this.state
+    return (
+      <Grid
+        container
+        direction="column"
+        justify="flex-start"
+        alignItems="center">
+        <Form
+          onSubmit={this.handleSubmit}
+          className="mt-5">
+          <h2 className="text-center">Edit the basics</h2>
+          <FormGroup>
+            <TextField
+              name="weight"
+              label="Your weight"
+              className="classes.textField mt-5"
+              margin="normal"
+              onChange={this.handleChange}
+              value={value}/>
+            <FormHelperText>Required*</FormHelperText>
+          </FormGroup>
+          <FormGroup>
+            <TextField
+              name="calorieGoal"
+              label="Calories per day"
+              className="classes.textField"
+              margin="normal"
+              type="number"
+              onChange={this.handleChange}
+              value={value}/>
+            <FormHelperText>Required*</FormHelperText>
+          </FormGroup>
+          <div className="mt-4 w-100">
+            <Button color="secondary">Let's get started!</Button>
+          </div>
+        </Form>
+      </Grid>
+    )
+  }
+}
